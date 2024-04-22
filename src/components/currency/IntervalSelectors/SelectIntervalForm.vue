bcec578add16ffdbfe2b95f55a02242301c7d36a
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-04-22 09:31:12 +0300

    fix: add separate state for start date and end date

--- src/components/currency/IntervalSelectors/SelectIntervalForm.vue
+++ src/components/currency/IntervalSelectors/SelectIntervalForm.vue
@@ -6,7 +6,8 @@ import DatePicker from '@/components/ui/DatePicker/DatePicker.vue'
 export default {
     data() {
         return {
-            date: dayjs()
+            startDate: dayjs(),
+            endDate: dayjs()
         }
     },
 
@@ -22,10 +23,10 @@ export default {
         <v-container>
             <v-row justify="space-between">
                 <v-col cols="12" sm="6" md="auto" class="flex-grow-1">
-                    <DatePicker v-model="date" label="Start date" />
+                    <DatePicker v-model="startDate" label="Start date" />
                 </v-col>
                 <v-col cols="12" sm="6" md="auto" class="flex-grow-1">
-                    <DatePicker v-model="date" label="End date" />
+                    <DatePicker v-model="endDate" label="End date" />
                 </v-col>
                 <v-col cols="12" md="3" class="d-flex flex-grow-1 justify-end justify-md-center">
                     <VBtn>
