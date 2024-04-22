4f88feec93c7eaa51890048c38b7a146cda026f8
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-04-22 17:40:21 +0300

    fix: interval form spacing

--- src/components/currency/IntervalSelectors/SelectIntervalForm.vue
+++ src/components/currency/IntervalSelectors/SelectIntervalForm.vue
@@ -19,23 +19,26 @@ export default {
 </script>
 
 <template>
-    <v-form>
-        <v-container>
-            <v-row justify="space-between">
-                <v-col cols="12" sm="6" md="auto" class="flex-grow-1">
-                    <DatePicker v-model="startDate" label="Start date" />
-                </v-col>
-                <v-col cols="12" sm="6" md="auto" class="flex-grow-1">
-                    <DatePicker v-model="endDate" label="End date" />
-                </v-col>
-                <v-col cols="12" md="3" class="d-flex flex-grow-1 justify-end justify-md-center">
-                    <VBtn>
-                        View report
-                    </VBtn>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-form>
+    <VForm class="form-container">
+        <VRow justify="space-between">
+            <VCol cols="12" sm="6" md="auto" class="flex-grow-1">
+                <DatePicker v-model="startDate" label="Start date" />
+            </VCol>
+            <VCol cols="12" sm="6" md="auto" class="flex-grow-1">
+                <DatePicker v-model="endDate" label="End date" />
+            </VCol>
+            <VCol cols="12" md="auto" class="d-flex justify-end">
+                <VBtn>
+                    View report
+                </VBtn>
+            </VCol>
+        </VRow>
+    </VForm>
 </template>
 
-<style lang="scss" scoped></style>
+
+<style scoped lang="scss">
+.form-container {
+    padding: 10px;
+}
+</style>
