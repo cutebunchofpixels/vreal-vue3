d200ef20aa25f6cc9d09f08d65e99882023d760d
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-05-07 11:47:06 +0300

    feat: not found page

---
+++ src/components/not-found/NotFoundPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="not-found-wrapper">
+        <div class="status">404</div>
+        <div class="message">Requested page does not exist</div>
+        <VBtn @click="$router.replace('/')">Back to index</VBtn>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    line-height: 1;
+    gap: 25px;
+}
+
+.status {
+    font-size: 96px;
+}
+
+.message {
+    font-size: 48px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from '@/components/layout/MainLayout.vue'
 import SigninPage from '@/components/auth/SigninPage.vue'
 import SignupPage from '@/components/auth/SignupPage.vue'
-import UsersPage from '@/components/users/UsersPage.vue'
+import NotFoundPage from '@/components/not-found/NotFoundPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,7 +32,17 @@ const router = createRouter({
         {
           path: '/users',
           name: 'users',
-          component: UsersPage
+          component: () => import('@/components/users/UsersPage.vue')
+        },
+        {
+          path: '/users/:id',
+          name: 'editUser',
+          component: () => import('@/components/users/EditUser/EditUserPage.vue')
+        },
+        {
+          path: '/:pathMatch(.*)*',
+          name: 'notFound',
+          component: NotFoundPage
         }
       ]
     }
